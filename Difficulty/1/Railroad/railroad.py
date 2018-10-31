353bb34ccc4000a4452a829a3a0d13169ec25011
Craigory V Coppola <craigorycoppola@gmail.com>
2018-10-31 09:18:33 -0400

    Roaming Romans and Railroad Slns

---
+++ Difficulty/1/Railroad/railroad.py
@@ -0,0 +1,7 @@
+#Railroad, OpenKattis, https://open.kattis.com/problems/railroad2
+#Sln by Craigory
+
+if int(input().split()[1])%2 == 0:
+	print("possible")
+else:
+	print("impossible")
---
+++ Difficulty/1/RoamingRomans/romans.py
@@ -0,0 +1,4 @@
+#Roaming Romans, OpenKattis, https://open.kattis.com/problems/romans
+#sln by Craigory
+import math
+print(math.floor(((1000*5280*eval(input())/4854)+.5)))
