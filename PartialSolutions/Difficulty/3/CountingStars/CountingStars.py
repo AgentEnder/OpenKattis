48e7aa087799c6531a1ca867b91c13cfc4d89577
Craigory V Coppola <agentendercake@gmail.com>
2019-02-07 16:39:20 -0500

    Add files via upload

--- PartialSolutions/Difficulty/3/CountingStars/CountingStars.py
+++ PartialSolutions/Difficulty/3/CountingStars/CountingStars.py
@@ -7,31 +7,34 @@ import sys
 #resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
 sys.setrecursionlimit(10**6)
 
+grid = []
 
-def findStar(curr_x, curr_y, w, h, grid, star = set()):
+def findStar(curr_x, curr_y, w, h, star = set()):
 	curr = curr_y*w+curr_x
+	grid[curr_y][curr_x] = "#"
 	if curr in star:
 		return(set())
 	star.add(curr)
 	if (curr_x > 0 and grid[curr_y][curr_x-1] == "-"): # Left Check
-		star.union(findStar(curr_x-1,curr_y, w, h, grid, star))
+		star.union(findStar(curr_x-1,curr_y, w, h, star))
 	if (curr_x < w-1 and grid[curr_y][curr_x+1] == "-"): # Right
-		star.union(findStar(curr_x+1,curr_y, w, h, grid, star))
+		star.union(findStar(curr_x+1,curr_y, w, h, star))
 	if (curr_y > 0 and grid[curr_y-1][curr_x] == "-"): # Up Check
-		star.union(findStar(curr_x,curr_y-1, w, h, grid, star))
+		star.union(findStar(curr_x,curr_y-1, w, h, star))
 	if (curr_y < h-1 and grid[curr_y+1][curr_x] == "-"): # Down Check
-		star.union(findStar(curr_x,curr_y+1, w, h, grid, star))
+		star.union(findStar(curr_x,curr_y+1, w, h, star))
 	return star
 
 case = 1
 	
 for line in sys.stdin:
 	height, width = [int(x) for x in line.split()]
-	grid = []
 	for y in range(height):
 		grid.append(list(input().strip()))
 	stars = []
 	star_count=0
+	
+	
 	for y, row in enumerate(grid):
 		for x, cell in enumerate(row):
 			if cell == "-":
@@ -40,8 +43,9 @@ for line in sys.stdin:
 					if curr in star:
 						break
 				else:
-					stars.append(findStar(x,y,width,height,grid, star = set()))
+					stars.append(findStar(x,y,width,height, star = set()))
 				
 	res = "Case " + str(case) +  ": " + str(len(stars))
 	print(res)
+	grid = []
 	case+=1
